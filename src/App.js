025fc0193b55b5585007e063aef9f6e865fb3576
Kate Johnson <johnson.kat.g@gmail.com>
2022-03-14 21:16:05 -0400

    added forms and styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layouts/MainNavigation'
+import Layout from './components/layouts/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
